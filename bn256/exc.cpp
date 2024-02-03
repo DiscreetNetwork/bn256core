a51ef9df1bee525b99f76c523e843d0823ff2c7d
Brandon Koerner <bksquared1024@gmail.com>
2024-02-03 12:25:08 -0500

    improve exc

--- bn256/exc.cpp
+++ bn256/exc.cpp
@@ -6,18 +6,23 @@ static char ms_GlobExcp[4096] = { 0 };
 static int ms_GlobExcpLen = 0;
 
 
-
+#if defined(_MSC_VER)
 class InternalException : public std::exception
 {
 public:
     InternalException(const char* err) : std::exception(err) {}
 };
+#endif
 
 void ThrowException(const char* s_Messg)
 {
     ms_GlobExcpLen = strlen(s_Messg);
     memcpy(ms_GlobExcp, s_Messg, ms_GlobExcpLen);
+#if defined(_MSC_VER)
     throw InternalException(s_Messg);
+#else
+    throw std::exception();
+#endif
 }
 
 
